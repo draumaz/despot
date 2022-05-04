1d9e2503a0ba4b8f568ebedf205b357faf15e102
draumaz <77967471+draumaz@users.noreply.github.com>
2022-05-04 12:32:44 -0700

    update packages, not repo list

--- bildo.sh
+++ bildo.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 PKG_LIST=$(ls pkg/src | tr " " "\n")
-REPO_LIST=$(ls pkg/repo | tr " " "\n")
 
 case "$1" in
 	"list")
@@ -17,7 +16,7 @@ case "$1" in
 		done
 		;;
 	"upgrade")
-		for l in $REPO_LIST; do
+		for l in $PKG_LIST; do
 			bash pkg/repo/$l
 		done
 		;;
