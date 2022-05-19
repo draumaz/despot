92acb7345c8d445db615b5c01ca6254693ef7263
draumaz <draumaz@goatopossum.com>
2022-05-19 06:18:15 -0700

    deps stuff

--- pkg/repo/ly.bash
+++ pkg/repo/ly.bash
@@ -27,10 +27,8 @@ function uninstall() {
 }
 
 function build() {
-  if [ ! "$(grep ID /etc/os-release | grep debian)" == "" ]; then
-    echo $DEB_DEPS | xargs sudo apt-get install -y
-  fi
-
+  if [ ! "$(grep -i debian /etc/os-release)" == "" ]; then echo $DEB_DEPS | xargs sudo apt-get install -y; fi
+  
   echo "working on: $PKG_NAME"
 
   cd $BUILD_DIR
