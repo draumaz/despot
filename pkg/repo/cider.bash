59f58b7e3443b86d8d62b565e5136d86f71dd475
draumaz <draumaz@goatopossum.com>
2022-07-02 19:17:54 -0700

    improve cider

--- pkg/repo/cider.bash
+++ pkg/repo/cider.bash
@@ -3,11 +3,11 @@
 PKG_REPO="https://github.com/ciderapp/cider-releases"
 PKG_NAME="cider"
 
-# this package has no dependencies.
+true << EOF
+DEB_DEPS wget curl libauthen-sasl-perl libclone-perl libdata-dump-perl libencode-locale-perl libfile-basedir-perl libfile-desktopentry-perl libfile-listing-perl libfile-mimeinfo-perl libfont-afm-perl libhtml-form-perl libhtml-format-perl libhtml-parser-perl libhtml-tagset-perl libhtml-tree-perl libhttp-cookies-perl libhttp-daemon-perl libhttp-date-perl libhttp-message-perl libhttp-negotiate-perl libio-html-perl libio-socket-ssl-perl libio-stringy-perl libipc-system-simple-perl liblwp-mediatypes-perl liblwp-protocol-https-perl libmailtools-perl libnet-dbus-perl libnet-http-perl libnet-smtp-ssl-perl libnet-ssleay-perl libnotify4 libsecret-1-0 libsecret-common libtie-ixhash-perl libtimedate-perl libtry-tiny-perl liburi-perl libwww-perl libwww-robotrules-perl libx11-protocol-perl libxml-parser-perl libxml-twig-perl libxml-xpathengine-perl perl-openssl-defaults xdg-utils
+EOF
 
 BUILD_DIR="pkg/src/$PKG_NAME"
-INSTALL_CMD="dpkg -i cider_"$LVER"_amd64.deb"
-
 
 function build() {
   printf "$PKG_NAME: working\n"
@@ -16,10 +16,12 @@ function build() {
   
   LVER="$(curl --silent --location --output /dev/null \
     --write-out "%{url_effective}\n" $PKG_REPO/releases/latest | cut -c58-)"
+  
+  if [ ! -e cider_"$LVER"_amd64.deb ]; then
+    wget "$PKG_REPO"/releases/download/v"$LVER"/cider_"$LVER"_amd64.deb
+  fi
 
-  wget "$PKG_REPO"/releases/download/v"$LVER"/cider_"$LVER"_amd64.deb
-
-  if sudo $INSTALL_CMD; then
+  if sudo dpkg -i cider_"$LVER"_amd64.deb; then
     printf "$PKG_NAME: installed.\n"
   else
     printf "$PKG_NAME: failed to install.\n"
