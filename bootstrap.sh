9ecd935d807fff7fb42d25e4f8cd4054d3dbc454
draumaz <77967471+draumaz@users.noreply.github.com>
2022-07-13 19:12:43 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 case "$(id -u)" in 1000) echo 'run me as root!'; exit ;; esac
 curl -fLO https://raw.githubusercontent.com/draumaz/despot/trunk/despot
-chmod +x despot
-mv -vf despot /usr/local/bin
+chmod +x despot.tmp
+mv -vf despot.tmp /usr/local/bin/despot
 echo 'make sure /usr/local/bin is in your PATH.'
