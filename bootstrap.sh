6adc3137e0e136cb3c338ed42b05aadbb3f22fe0
draumaz <77967471+draumaz@users.noreply.github.com>
2022-07-13 20:23:41 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-case "$(id -u)" in 1000) echo 'run me as root!'; exit ;; esac
+case "$(id -u)" in 1000) echo 'run me as root!' && exit ;; esac
 curl https://raw.githubusercontent.com/draumaz/despot/trunk/despot > despot.tmp
 chmod +x despot.tmp
 mv -vf despot.tmp /usr/local/bin/despot
