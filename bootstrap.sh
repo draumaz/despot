ec320952b87a722c460e2ff3ff5a8a6e078ac929
draumaz <77967471+draumaz@users.noreply.github.com>
2022-07-14 19:50:28 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 case "$(id -u)" in 1000) echo 'run me as root!' && exit ;; esac
-echo "-> downloading despot binary"
-curl --silent https://raw.githubusercontent.com/draumaz/despot/master/despot > despot.tmp
-echo "-> making binary executable"
-chmod +x despot.tmp
+curl https://raw.githubusercontent.com/draumaz/despot/master/despot > despot.tmp
+chmod -vf +x despot.tmp
 mv -vf despot.tmp /usr/local/bin/despot
 echo '!! make sure /usr/local/bin is in your PATH.'
