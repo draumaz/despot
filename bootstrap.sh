0555180fd69b458147962407a6ae56c152e18947
draumaz <draumaz@goatopossum.com>
2022-07-17 13:06:40 +0000

    clarité

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 case "$(id -u)" in 1000) echo 'run me as root!' && exit ;; esac
 echo "-> downloading despot" && curl --silent https://raw.githubusercontent.com/draumaz/despot/master/despot > despot.tmp
-echo -n "-> installing "
 chmod -f +x despot.tmp
 mv -f despot.tmp /usr/local/bin/despot
-echo 'done.'
+echo '-> installed.'
 echo '!! make sure /usr/local/bin is in your PATH.'
